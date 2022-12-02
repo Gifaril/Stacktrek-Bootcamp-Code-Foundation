192a36e96a066e1b22e85b114f24f548d5a9a48d
Gifaril Mae Nisperos <111416263+Gifaril@users.noreply.github.com>
2022-12-02 10:17:56 +0800

    Algo Exercise

---
+++ Lesson11_SortingAlgo/11A/1_Form_Your_Line.py
@@ -0,0 +1,9 @@
+def sort(elements):
+    for i in range(1,len(elements)):
+        j = i-1
+        nxt = elements[i]
+        while(elements[j] > nxt) and (j >= 0):
+            elements[j+1] = elements[j]
+            j = j-1
+        elements[j+1] = nxt
+    return elements
