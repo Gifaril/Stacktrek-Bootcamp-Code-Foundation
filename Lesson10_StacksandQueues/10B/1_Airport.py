2cc3fd4b2880b7ab0cf32d4e2819de7c9038a28a
Gifaril Mae Nisperos <111416263+Gifaril@users.noreply.github.com>
2022-12-02 10:10:15 +0800

    10B Exercise

---
+++ Lesson10_StacksandQueues/10B/1_Airport.py
@@ -0,0 +1,44 @@
+import heapq
+
+class Airport:
+    def __init__(self):
+        self.data = []
+        heapq.heapify(self.data)
+
+    def add_passenger(self, passenger):
+        heapq.heappush(self.data, passenger)
+
+    def dequeue(self):
+      	return heapq.heappop(self.data)
+
+
+class Passenger:
+    def __init__(self, name, age, is_pregnant, with_children):
+        self.name = name
+        self.age = age
+        self.is_pregnant = is_pregnant
+        self.with_children = with_children
+
+    def sub_group(self):
+        if self.age >= 60:
+            return 1
+        elif self.is_pregnant or self.with_children:
+            return 2
+        return 3
+
+    def __repr__(self):
+        return self.name
+
+    def __lt__(self, other):
+        return self.sub_group() < other.sub_group()
+
+
+airport = Airport()
+passenger1 = Passenger("Henry", 27, False, False)
+passenger2 = Passenger("Jen", 25, True, False)
+passenger3 = Passenger("Nica", 67, False, False)
+
+airport.add_passenger(passenger1)
+airport.add_passenger(passenger2)
+airport.add_passenger(passenger3)
+print(airport.dequeue())
---
+++ Lesson10_StacksandQueues/10B/3_Saseiko.py
@@ -0,0 +1,22 @@
+import heapq
+from datetime import datetime
+
+class Saseiko:
+    def __init__(self):
+        self.data = list()
+        heapq.heapify(self.data)
+
+    def add_activity(self, activity):
+        heapq.heappush(self.data, activity)
+
+    def most_urgent_activity(self):
+        return heapq.heappop(self.data)
+
+
+class Activity:
+    def __init__(self, activity_title, deadline):
+        self.activity_title = activity_title
+        self.deadline = deadline
+
+    def __lt__(self, other):
+        return self.deadline < other.deadline
